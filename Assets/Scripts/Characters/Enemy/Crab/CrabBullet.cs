0cb704eb482bede69a3a68b9f0a8b32af4ddb32b
Chris <chris.jumpbuttongames@gmail.com>
2020-10-29 14:41:36 +0000

    UPDATE crab enemy has new scripts specific to it. Changes to shoot time behaviour

---
+++ Assets/Scripts/Characters/Enemy/Crab/CrabBullet.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class CrabBullet : MonoBehaviour
+{
+	[Header("Bullet Movement")]
+
+	[Tooltip("The velocity of the bullet")]
+	[SerializeField] 
+	float speed = 5f;
+
+
+	[Header("Bullet Damage Dealt")]
+
+	[Tooltip("The amount of damage the bullet does to the recieving character")]
+	[SerializeField] 
+	int damage = 20;
+
+
+	[Header("Player Hit Detection")]
+
+	[Tooltip("Tracks whether the enemey bullet hit the player or not")]
+	public static bool hitPlayer = false;
+
+
+	[Header("Bullet Physics")]
+
+	[Tooltip("Attach the rigidbody of the bullet here")]
+	public Rigidbody2D rb2d;
+
+
+	[Header("Impact Effect")]
+
+	[Tooltip("The object which contains the bullet impact effect animation")]
+	public GameObject bulletImpactEffect;
+
+	void Start()
+	{
+		rb2d.velocity = transform.right * speed;
+	}
+
+	// For later on, have a onCollide method which checks if the walls or ceiling was hit
+	// then play impact animation and destroy the bullet
+
+	void OnTriggerEnter2D(Collider2D hitInfo)
+	{
+		PlayerHealth player = hitInfo.GetComponent<PlayerHealth>();
+
+		if (player != null)
+		{
+			hitPlayer = true;
+			player.TakeDamage(damage);
+
+			Instantiate(bulletImpactEffect, transform.position, transform.rotation);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Characters/Enemy/Crab/CrabShoot.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class CrabShoot : MonoBehaviour
+{
+	[Header("Shooting Controls")]
+
+	[Tooltip("Tracks whether the crab is allowed shoot or not")]
+	[SerializeField]
+	bool canShoot = false;
+
+	[Tooltip("Determines when the crabs shooting behaviour will start")]
+	[SerializeField]
+	float timeUntilShootingStarts = 2f;
+
+	[Tooltip("Determines how often the crab will shoot. E.G shoot every X amount of seconds.")]
+	[SerializeField]
+	float rateOfShooting = 2f;
+
+	[Tooltip("Determines from what distance the crab can see the player")]
+	[SerializeField]
+	float shotDectectionRange = 10f;
+
+
+	[Header("Bullet Controls")]
+
+	[Tooltip("The crab bullet prefab")]
+	public GameObject crabBulletPrefab;
+
+	[Tooltip("Point from where the crab bullet fires.")]
+	public Transform crabFirePoint;
+
+	void Start()
+	{
+		InvokeRepeating("AllowedShoot", timeUntilShootingStarts, rateOfShooting);
+	}
+
+	void Update()
+	{
+		RaycastHit2D playerInfo = Physics2D.Raycast(crabFirePoint.position, crabFirePoint.right, shotDectectionRange);
+
+		if (playerInfo.collider == true && playerInfo.collider.gameObject.tag == "Player")
+		{
+			if (canShoot)
+			{
+				Shoot();
+				canShoot = false;
+			}
+		}
+	}
+
+	// Shoot Methods
+
+	void AllowedShoot()
+	{
+		canShoot = true;
+	}
+
+	void Shoot()
+	{
+		Instantiate(crabBulletPrefab, crabFirePoint.position, crabFirePoint.rotation);
+	}
+
+	
+}
